1bd0868412089e15104fc67de0e2f93951eab90c
FranParraH <156036503+FranParraH@users.noreply.github.com>
2024-01-13 14:52:26 +0000

    actualizacion

--- src/app.py
+++ src/app.py
@@ -3,6 +3,10 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 todos = [ { "label": "My first task", "done": False } ]
+user = [{ "Name": "Ximena", "edad": 26 },
+        { "Name": "Javier", "edad": 32 },
+        ]
+
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
@@ -21,6 +25,14 @@ def delete_todo(position):
     todos.pop(position)
     return jsonify(todos)
 
+@app.route('/user/<int:position2>', methods=['GET'])
+def get_user(position2):
+    print("This is the position to get:", position2)
+    get_user_from_list = user[position2]
+    if get_user_from_list is not None:
+        return jsonify(get_user_from_list)
+    else:
+        return jsonify({"error": "usuario no encontrado"}), 404
 
 
 
